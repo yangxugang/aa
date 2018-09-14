fbfca81ffe2605c6d821da8cd887c84eb12bf7ae
bigshowyang <30277471+bigshowyang@users.noreply.github.com>
2018-09-14 10:55:45 +0800

    commit1

---
+++ src/t1/c1.java
@@ -0,0 +1,15 @@
+package t1;
+
+public class c1 {
+
+	public c1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(1111111);
+
+	}
+
+}
